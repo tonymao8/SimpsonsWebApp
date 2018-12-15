79728a549988ec19db1c4fb208359714aa702c7e
Tony Mao <tonymao458@gmail.com>
2018-12-15 17:12:30 +1100

    added alt prop for image

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,10 +2,9 @@ import React from 'react';
 
 class ImageList extends React.Component {
   state = { data: [] };
-  //We want Character Direction right to be first
-  sortImages() {
-    const images = this.props.images;
-    images.sort(function(a, b) {
+  //We want character direction 'right' to be first
+  sortImages(image_list) {
+    return image_list.sort(function(a, b) {
       if (a.characterDirection < b.characterDirection) {
         return 1;
       }
@@ -14,24 +13,24 @@ class ImageList extends React.Component {
       }
       return 0;
     });
-    this.setState({ data: images });
   }
 
   render() {
-    const imageCards = this.state.data.map(image => {
+    const images = this.props.images;
+    var imageCards = this.sortImages(images);
+    imageCards = imageCards.map(image => {
       return (
         <div>
           <img
             style={{ maxWidth: '10%', maxHeight: '10%' }}
             src={image.image}
+            alt="Simpson Character"
           />
           <text>{image.quote}</text>'
         </div>
       );
     });
-
     return <div> {imageCards} </div>;
   }
 }
-
 export default ImageList;
