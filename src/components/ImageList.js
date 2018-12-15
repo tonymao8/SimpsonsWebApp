56559e64de6b11b278ddd4f308b775c3ddbfc9c9
Tony Mao <tonymao458@gmail.com>
2018-12-15 16:08:18 +1100

    refactored to not modify props directly

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,26 +1,37 @@
 import React from 'react';
 
-const ImageList = props => {
+class ImageList extends React.Component {
+  state = { data: [] };
   //We want Character Direction right to be first
-  props.images.sort(function(a, b) {
-    if (a.characterDirection < b.characterDirection) {
-      return 1;
-    }
-    if (a.characterDirection > b.characterDirection) {
-      return -1;
-    }
-    return 0;
-  });
-  const imageCards = props.images.map(image => {
-    return (
-      <div>
-        <img style={{ maxWidth: '10%', maxHeight: '10%' }} src={image.image} />
-        <text>{image.quote}</text>'
-      </div>
-    );
-  });
+  sortImages() {
+    const images = this.props.images;
+    images.sort(function(a, b) {
+      if (a.characterDirection < b.characterDirection) {
+        return 1;
+      }
+      if (a.characterDirection > b.characterDirection) {
+        return -1;
+      }
+      return 0;
+    });
+    this.setState({ data: images });
+  }
 
-  return <div> {imageCards} </div>;
-};
+  render() {
+    const imageCards = this.state.data.map(image => {
+      return (
+        <div>
+          <img
+            style={{ maxWidth: '10%', maxHeight: '10%' }}
+            src={image.image}
+          />
+          <text>{image.quote}</text>'
+        </div>
+      );
+    });
+
+    return <div> {imageCards} </div>;
+  }
+}
 
 export default ImageList;
