685213e5c66583a212cf62cf648818e71ad31f25
Tony Mao <tonymao458@gmail.com>
2018-12-15 14:30:34 +1100

    sort done

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,16 +1,8 @@
 import React from 'react';
 
 const ImageList = props => {
-  const imageCards = props.images.map(image => {
-    return (
-      <div>
-        <img style={{ maxWidth: '10%', maxHeight: '10%' }} src={image.image} />
-        <text>{image.quote}</text>'
-      </div>
-    );
-  });
   //We want Character Direction right to be first
-  imageCards.sort(function(a, b) {
+  props.images.sort(function(a, b) {
     if (a.characterDirection < b.characterDirection) {
       return 1;
     }
@@ -18,6 +10,14 @@ const ImageList = props => {
       return -1;
     }
     return 0;
+  });
+  const imageCards = props.images.map(image => {
+    return (
+      <div>
+        <img style={{ maxWidth: '10%', maxHeight: '10%' }} src={image.image} />
+        <text>{image.quote}</text>'
+      </div>
+    );
   });
 
   return <div> {imageCards} </div>;
