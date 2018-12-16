98576fbc5534d9b4e9e3a7832b7467cafe9992d4
Tony Mao <tonymao458@gmail.com>
2018-12-17 10:17:11 +1100

    refactor sortImages to prevent blockage of re-render

--- src/components/App.js
+++ src/components/App.js
@@ -5,35 +5,41 @@ import '../styles/scss/App.scss';
 
 class App extends React.Component {
   state = {
-    images: []
+    images: {
+      left: [],
+      right: []
+    }
   };
   getQuotes = async term => {
     const response = await simpsons.get();
-    this.setState({ images: response.data });
+    var data = this.sortImages(response.data);
+    this.setState({
+      images: {
+        left: data.left,
+        right: data.right
+      }
+    });
   };
 
-  sortImages() {
-    const leftList = this.state.images.filter(
-      word => word.characterDirection === 'Left'
-    );
-    const rightList = this.state.images.filter(
+  sortImages(images) {
+    const leftList = images.filter(word => word.characterDirection === 'Left');
+    const rightList = images.filter(
       word => word.characterDirection === 'Right'
     );
     return { left: leftList, right: rightList };
   }
 
   render() {
-    var data = this.sortImages(this.state.images);
     return (
       <div className="container">
         <button className="btn" onClick={this.getQuotes}>
           Get Quotes
         </button>
         <div className="container--left">
-          <ImageList images={data.left} />
+          <ImageList images={this.state.images.left} />
         </div>
         <div className="container--right">
-          <ImageList images={data.right} />
+          <ImageList images={this.state.images.right} />
         </div>
       </div>
     );
