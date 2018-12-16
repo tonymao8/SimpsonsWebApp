43807ac0681f2f70948dbdd4f4cbb419d6d793b6
Tony Mao <tonymao458@gmail.com>
2018-12-16 15:38:52 +1100

    migrated style to scss for all components

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,45 @@
 import React from 'react';
 import simpsons from '../api/simpsons';
 import ImageList from './ImageList';
-import '../styles/App.scss';
+import '../styles/scss/App.scss';
 
 class App extends React.Component {
-  state = { images: [] };
-  onClick = async term => {
+  state = {
+    images: [],
+    data: {
+      left: [],
+      right: []
+    }
+  };
+  renderContent = async term => {
     const response = await simpsons.get();
     this.setState({ images: response.data });
-    //this.forceUpdate();
   };
 
+  sortImages(image_list) {
+    var arrayLength = image_list.length;
+    for (let i = 0; i < arrayLength; i++) {
+      if (image_list[i].characterDirection === 'Left') {
+        this.state.data.left.push(image_list[i]);
+      } else {
+        this.state.data.right.push(image_list[i]);
+      }
+    }
+  }
+
   render() {
+    const images = this.state.images;
+    this.sortImages(images);
     return (
-      <div id="parent">
-        <div
-          className="ui left aligned container"
-          style={{ marginTop: '10px', maxWidth: '50%' }}
-        >
-          <button className="ui primary button" onClick={this.onClick}>
-            Get Quotes
-          </button>
-          <ImageList images={this.state.images} />
+      <div className="container">
+        <button className="btn" onClick={this.renderContent}>
+          Get Quotes
+        </button>
+        <div className="container--left">
+          <ImageList images={this.state.data.left} />
         </div>
-        <div
-          className="ui right aligned container"
-          style={{ marginTop: '10px', maxWidth: '50%' }}
-        >
-          Done
+        <div className="container--right">
+          <ImageList images={this.state.data.right} />
         </div>
       </div>
     );
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,28 +1,13 @@
 import React from 'react';
+import '../styles/scss/App.scss';
 
 class ImageList extends React.Component {
-  state = { data: [] };
-  //We want character direction 'right' to be first
-  sortImages(image_list) {
-    return image_list.sort(function(a, b) {
-      if (a.characterDirection < b.characterDirection) {
-        return 1;
-      }
-      if (a.characterDirection > b.characterDirection) {
-        return -1;
-      }
-      return 0;
-    });
-  }
-
   render() {
-    const images = this.props.images;
-    var imageCards = this.sortImages(images);
-    imageCards = imageCards.map(image => {
+    var imageCards = this.props.data.map(image => {
       return (
-        <div>
+        <div className="card">
           <img
-            style={{ maxWidth: '10%', maxHeight: '10%' }}
+            className="card__img"
             src={image.image}
             alt="Simpson Character"
           />
